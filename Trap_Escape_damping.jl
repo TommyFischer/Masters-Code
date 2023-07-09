10dc5c6267c24e1608d8c8664854633f50aa94f8
TommyFischer <tommyzfischer@gmail.com>
2023-07-09 14:28:00 +1200

    modified:   Trap_Escape_damping.jl

--- Trap_Escape_damping.jl
+++ Trap_Escape_damping.jl
@@ -22,12 +22,12 @@ g = 4π*ħ^2*a_s/m
 τ = ħ/μ
 
 const L = (40,30,20)     # Condensate size
-const M = (256,256,256)  # System Grid
+const M = (300,300,300)  # System Grid
 
 A_V = 30    # Trap height
 n_V = 24    # Trap Power (pretty much always 24)
-L_V = 5     # No. of healing lengths for V to drop from A_V to 0.01A_V 
-L_P = 15    # Amount of padding outside trap (for expansion)
+L_V = 3     # No. of healing lengths for V to drop from A_V to 0.01A_V 
+L_P = 6    # Amount of padding outside trap (for expansion)
 
 L_T = L .+ 2*(L_P + L_V)  # Total grid size
 use_cuda = CUDA.functional()
@@ -70,7 +70,9 @@ V_D = 2.5
 
 V_damp = -im * V_D * [(abs(i) > 0.5*L[1] + L_V + δ) || (abs(j) > 0.5*L[2] + L_V + δ) || (abs(k) > 0.5*L[3] + L_V + δ) ? 1 : 0 for i in Array(X[1]), j in Array(X[2]), k in Array(X[3])] |> cu;
 
-function Escape_GPE!(dψ,ψ,var,t)
+function Escape_VPE!(dψ,ψ,var,t)
+    #GC.gc([full=false])
+    #CUDA.memory_status()
     kfunc_opt!(dψ,ψ)
     @. dψ = -im*(0.5*dψ + (V_0 + $V(t) + V_damp + abs2(ψ) - 1)*ψ)
 end
@@ -79,13 +81,13 @@ Shake_params = Dict(
     "title" => "EscapeTurb $M, $L",
     "ψ" => ψ_GS,
     "γ" => [0],
-    "tf" => [6.0/τ],
+    "tf" => [3.0],
     "Nt" => 100,
-    "Shake_Grad" => [0.1]
+    "Shake_Grad" => [0.15]
 ) |> dict_list;   
 
 function save_func(res,d)
-    wsave("/nesi/nobackup/uoo03837/results2/" * savename(d,"jld2"),
+    wsave("/nesi/nobackup/uoo03837/results3/" * savename(d,"jld2"),
     Dict("res" => res,
         "X" => Array.(X),
         "K" => Array.(K),
@@ -95,6 +97,7 @@ function save_func(res,d)
         "M" => M,
         "L" => L
     ))
+    global ψ_turb = res[end] |> cu
 end;
 
 for (i,d) in enumerate(Shake_params)
@@ -105,7 +108,46 @@ for (i,d) in enumerate(Shake_params)
     global V(t) = sin(ω_shake*t)*shakegrid
 
     res = []
-    GPU_Solve!(res,Escape_GPE!,ψ_GS,LinRange(0,tf,Nt),γ,alg=Tsit5(),plot_progress=false)
-    #push!(TURB_RES,res)
+    GPU_Solve!(res,Escape_VPE!,ψ_GS,LinRange(0,tf,Nt),γ,alg=Tsit5(),plot_progress=false)
     save_func(res,d)
 end
+
+    #------------------------------- Relaxation ------------------------------
+
+    Relax_params = Dict(
+    "title" => "Relax $M, $L",
+    "ψ" => ψ_turb,
+    "γ" => [0],
+    "tf" => [2.0],
+    "Nt" => 100,
+) |> dict_list;   
+
+    function save_func(res,d)
+        wsave("/nesi/nobackup/uoo03837/results3/" * savename(d,"jld2"),
+        Dict("res" => res,
+            "X" => Array.(X),
+            "K" => Array.(K),
+            "dX" => dX,
+            "dK" => dK,
+            "V" => Array(V_0),
+            "M" => M,
+            "L" => L
+        ))
+        ψ_turb = res[end] |> cu
+    end;
+
+    function Escape_GPE!(dψ,ψ,var,t)
+        #GC.gc([full=false])
+        #CUDA.memory_status()
+        kfunc_opt!(dψ,ψ)
+        @. dψ = -im*(0.5*dψ + (V_0 + V_damp + abs2(ψ) - 1)*ψ)
+    end
+
+    for (i,d) in enumerate(Relax_params)
+        @unpack ψ, γ, tf, Nt = d
+            
+        res = []
+        GPU_Solve!(res,Escape_VPE!,ψ_GS,LinRange(0,tf,Nt),γ,alg=Tsit5(),plot_progress=false)
+        save_func(res,d)
+    end
+
