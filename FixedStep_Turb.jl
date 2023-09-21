f07ab99b28dbc3a27b63df12bc3b8f7781769d13
TommyFischer <tommyzfischer@gmail.com>
2023-09-21 12:04:24 +1200

    modified:   FixedStep_Turb.jl

--- FixedStep_Turb.jl
+++ FixedStep_Turb.jl
@@ -43,8 +43,8 @@ end
 end
 
 #ψ_rand = adapt(CuArray,load("/nesi/nobackup/uoo03837/Final_res/Final_Grid_GS/ψ_01_GS")["psi"])
-#ψ_rand = adapt(CuArray,load("/home/fisto108/ψ_01_GS")["psi"])
-ψ_rand = adapt(CuArray,randn(M) .+ im*randn(M)  .|> abs |> complex); # Initial State
+ψ_rand = adapt(CuArray,load("/home/fisto108/ψ_01_GS")["psi"])
+#ψ_rand = adapt(CuArray,randn(M) .+ im*randn(M)  .|> abs |> complex); # Initial State
 
 begin # Arrays
     X,K,k2 = MakeArrays(L_T,M,use_cuda = false); # need to change V5 to allow kwarg
